101a94c4dcca2401cf4a359ce0868b0204a2a53a
Lucas Bulczak <lucas.bulczak@gmail.com>
2021-01-06 14:13:21 +0100

    Test creation of JBlock

--- src/test/java/com/epam/prejap/tetris/block/JBlockTest.java
+++ src/test/java/com/epam/prejap/tetris/block/JBlockTest.java
@@ -1,9 +1,27 @@
 package com.epam.prejap.tetris.block;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import static org.testng.Assert.*;
 
 @Test(groups = "Block")
 public class JBlockTest {
+    @Test
+    public void shallCreateJBlockWithCorrectDimensions() {
+        //given
+        SoftAssert softAssert = new SoftAssert();
+        Block jBlock = new JBlock();
+        byte[][] expectedImage = new byte[3][2];
+
+        //when
+        int actualRows = jBlock.rows;
+        int actualCols = jBlock.cols;
+
+        //then
+        softAssert.assertTrue(actualRows == expectedImage.length);
+        softAssert.assertTrue(actualCols == expectedImage[0].length);
+        softAssert.assertAll("Shall create J block with correct dimensions, but did not");
+    }
 }
