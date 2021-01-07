820713be87084909168f2145ac3057e5efca2dff
Lucas Bulczak <lucas.bulczak@gmail.com>
2021-01-07 23:42:30 +0100

    Change the JavaDoc to describe current implementation

--- src/test/java/com/epam/prejap/tetris/block/BlockFeedTestFactory.java
+++ src/test/java/com/epam/prejap/tetris/block/BlockFeedTestFactory.java
@@ -3,16 +3,16 @@ package com.epam.prejap.tetris.block;
 import org.testng.annotations.Factory;
 
 /**
- * Performing the same tests for each new block causes code duplication. To avoid this, a method has been created
- * that supplies the simple name of the underlying class as a parameter to {@link BlockFeedTest} class.
+ *  Performing the same tests for each new block causes code duplication. To avoid this, a method has been created
+ *  that supplies {@link BlockFeedTest} class with concrete types of a {@link Block} class.
  * <p>
- * Thanks to this structure, each test from the BlockFeedTest class will be performed for each object provided
+ * Thanks to this structure, each test from the BlockFeedTest class will be performed for each type provided
  * by the {@link #factoryMethod()} below.
  * </p><p>
- * Adding tests for a new type of Block requires adding a new object in the method below.
+ * Adding tests for a new type of Block requires adding a new type in the method below.
  * </p><p>
  * Sample implementation:<br>
- * <pre>{@code new BlockFeedTest(<NEW_BLOCK_CLASS_NAME>.class.getSimpleName()) }</pre>
+ * <pre>{@code new BlockFeedTest(<NEW_BLOCK_CLASS_NAME>.class) }</pre>
  *
  * @author Łukasz Bulczak
  * @see Block
