5bd59cbdf26dd6ac227426bbdce7f2aa95ff232b
Lucas Bulczak <lucas.bulczak@gmail.com>
2021-01-08 22:07:48 +0100

    Add IBlock to BlockFeedTestFactory

--- src/test/java/com/epam/prejap/tetris/block/BlockFeedTestFactory.java
+++ src/test/java/com/epam/prejap/tetris/block/BlockFeedTestFactory.java
@@ -25,6 +25,7 @@ public class BlockFeedTestFactory {
                 new BlockFeedTest(LBlock.class),
                 new BlockFeedTest(OBlock.class),
                 new BlockFeedTest(JBlock.class),
+                new BlockFeedTest(IBlock.class),
         };
     }
 }
